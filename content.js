7dc78faa7d429d862937f2b36520ae4f7edbd4fd
Evan Gubarev <evchik2007@gmail.com>
2020-02-19 21:26:56 -0800

    made the svue login dark  mode freindly

--- content.js
+++ content.js
@@ -2,3 +2,5 @@ document.body.style.backgroundColor="#323232";
 document.getElementById("maincontent").style.backgroundColor = "#333333";
 document.getElementById("Greeting").style.color = 'white';
 document.getElementById("DistrictName").style.color = 'white';
+document.getElementById("LoginSVUE").style.color = 'white';
+

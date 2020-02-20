b9f87df17834b7d99abc6a2ac12f2e0344c9f7b0
Evan Gubarev <evchik2007@gmail.com>
2020-02-19 20:46:35 -0800

    make stuff work

--- content.js
+++ content.js
@@ -1,4 +1,2 @@
 document.body.style.backgroundColor="#323232"
 document.getElementById("maincontent").style.backgroundColor = "#333333";
-document.getElementById("student-id").style.backgroundColor = "#333333";
-

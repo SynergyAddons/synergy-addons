04637c40e68686c52034f7a2210f716a03b061fb
Evan Gubarev <evchik2007@gmail.com>
2020-02-19 20:39:20 -0800

    Update content.js

--- content.js
+++ content.js
@@ -1,3 +1,4 @@
 document.body.style.backgroundColor="#323232"
 document.getElementById("maincontent").style.backgroundColor = "#333333";
-document.getElementById("ctl00_ctl00_MainContent_PXPMainContent_repStudentSummaries_ctl00_StudentSummaryItem_StudentSummary").style.backgroundColor = "#333333";
+document.getElementById("student-id").style.backgroundColor = "#333333";
+

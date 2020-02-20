96012b664a786824ae7c17ed801acbb6e45da8b1
Evan Gubarev <evchik2007@gmail.com>
2020-02-19 18:12:41 -0800

    Added main files.
    Very basic features included

---
+++ content.js
@@ -0,0 +1,2 @@
+document.body.style.backgroundColor="#525c3e"
+document.getElementById("maincontent").style.backgroundColor = "lightblue";
---
+++ popup.js
@@ -0,0 +1,13 @@
+  let sourceclicked = document.getElementById('source');
+
+
+sourceclicked.onclick = function(element) {
+   chrome.windows.create({
+  url: "https://github.com/gubareve/synergy-addons",
+  type: "popup",
+  focused: true
+});
+    
+  };
+
+

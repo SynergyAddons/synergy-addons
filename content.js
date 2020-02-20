b09ba73bedaefd2ec8f16dfa68530052e6bffe52
Evan Gubarev <evchik2007@gmail.com>
2020-02-19 20:35:49 -0800

    Update content.js

--- content.js
+++ content.js
@@ -1,2 +1,3 @@
-document.body.style.backgroundColor="#525c3e"
-document.getElementById("maincontent").style.backgroundColor = "lightblue";
+document.body.style.backgroundColor="#323232"
+document.getElementById("maincontent").style.backgroundColor = "#333333";
+document.getElementById("ctl00_ctl00_MainContent_PXPMainContent_repStudentSummaries_ctl00_StudentSummaryItem_StudentSummary").style.backgroundColor = "#333333";

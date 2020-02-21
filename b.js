6c6d1abb14f34411c237289de8bdde47ae8bab95
thexpiredpear <49598383+thexpiredpear@users.noreply.github.com>
2020-02-20 20:02:56 -0800

    Update b.js

--- b.js
+++ b.js
@@ -1,1 +1,1 @@
-document.styleSheets[0]('.UnreadMessages:hover { background-color: #808080; }', document.styleSheets[0].cssRules.length);
+document.styleSheets[0]('.UnreadMessage:hover { background-color: #808080; }', document.styleSheets[0].cssRules.length);

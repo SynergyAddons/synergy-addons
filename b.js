2668431901c9f9866ab838e7c734a42e13497bbf
thexpiredpear <49598383+thexpiredpear@users.noreply.github.com>
2020-02-21 09:01:49 -0800

    Update b.js

--- b.js
+++ b.js
@@ -1,4 +1,11 @@
-var messages = document.getElementsByClassName("UnreadMessage");
+var messages = document.getElementsByClassName("UnreadMessages");
 for (i = 0; i < messages.length; i++) {
-messages[i].setAttribute('style', 'background-color: #323232');
+messages[i].addEventListener("onmouseover", function(){
+  messages[i].setAttribute('style', 'background-color: #A9A9A9');
+});
+};
+
+var content = document.getElementsByClassName("MessageContent");
+for (i = 0; i < content.length; i++) {
+content[i].setAttribute('style', 'background-color: #808080');
 };

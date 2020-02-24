b515b12a474847c5e6d0b5cc0bf5efd1611b67ec
thexpiredpear <49598383+thexpiredpear@users.noreply.github.com>
2020-02-24 11:18:45 -0800

    Update a.js

--- a.js
+++ a.js
@@ -1,2 +1,9 @@
-document.getElementsByClassName("gb-student-assignments-grid")[0].style.color = 'black';
+chrome.storage.local.get('enabled', data => {
+    if (data.enabled) {
+        document.getElementsByClassName("gb-student-assignments-grid")[0].style.color = 'black';
 document.getElementById("gb-status").style.backgroundColor = 'white';
+    } else {
+        //it is disabled
+    }
+});
+

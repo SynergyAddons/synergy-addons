f40047d6609bc27214a50118cd364318d1243ee8
thexpiredpear <49598383+thexpiredpear@users.noreply.github.com>
2020-02-24 22:06:22 -0800

    Update studentid.js

--- studentid.js
+++ studentid.js
@@ -1,6 +1,4 @@
-
 var studentid = document.getElementsByClassName("student-id");
 studentid[0].style.display = 'none'; //turns out apparently u need an array for class names :/ - RP
 var info = document.getElementsByClassName("info");
 info[0].style.display = 'none'; //turns out apparently u need an array for class names :/ - RP
-

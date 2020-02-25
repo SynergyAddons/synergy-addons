996a2d750c852538113763cea8dbb57a5c347023
thexpiredpear <49598383+thexpiredpear@users.noreply.github.com>
2020-02-24 22:03:27 -0800

    Update districtname.js

--- districtname.js
+++ districtname.js
@@ -1,5 +1,4 @@
-chrome.storage.local.get(['enabled'], function(data) {
-    if (data.enabled) {
+
 var name = document.getElementById('DistrictName');
 name.setAttribute('style', 'color: #22bab0');
-    };
+

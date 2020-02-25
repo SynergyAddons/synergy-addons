4a668edfc8e8ddccec7ff1c17c81fb523aed5701
thexpiredpear <49598383+thexpiredpear@users.noreply.github.com>
2020-02-24 18:45:38 -0800

    Update calender.js

--- calender.js
+++ calender.js
@@ -1,2 +1,5 @@
+chrome.storage.local.get(['enabled'], function(data) {
+    if (data.enabled) {
 document.getElementsByClassName("cal_tbl")[0].style.color = 'white';
 document.getElementsByClassName("MessageContent")[0].style.backgroundcolor = '#333333';
+    };

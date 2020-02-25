4300c0cacbc1532d10799003a2297989fc5365e4
thexpiredpear <49598383+thexpiredpear@users.noreply.github.com>
2020-02-24 18:44:00 -0800

    Update a.js

--- a.js
+++ a.js
@@ -1,4 +1,4 @@
-chrome.storage.local.get(['enabled'], function(result) {
+chrome.storage.local.get(['enabled'], function(data) {
     if (data.enabled) {
         document.getElementsByClassName("gb-student-assignments-grid")[0].style.color = 'black';
 document.getElementById("gb-status").style.backgroundColor = 'white';

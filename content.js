6558eb714c4a8910e83ef4cf38ac2fbca37b92ff
thexpiredpear <49598383+thexpiredpear@users.noreply.github.com>
2020-02-24 18:46:05 -0800

    Update content.js

--- content.js
+++ content.js
@@ -1,6 +1,9 @@
+chrome.storage.local.get(['enabled'], function(data) {
+    if (data.enabled) {
 document.body.style.backgroundColor="#323232";
 document.getElementById("maincontent").style.backgroundColor = "#333333";
 document.getElementById("ep_StudentAssessments").style.color = "white";
 document.getElementById("DistrictName").style.color = 'white';
 document.getElementsByClassName("gb-student-assignments-grid")[0].style.color = 'black';
 document.getElementById("gb-status").style.backgroundColor = 'white';
+    };

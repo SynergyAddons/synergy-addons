26f609e4542f43bb606ad8a04da30ba250b8df47
thexpiredpear <49598383+thexpiredpear@users.noreply.github.com>
2020-02-24 21:59:59 -0800

    Update a.js

--- a.js
+++ a.js
@@ -1,8 +1,6 @@
-chrome.storage.local.get(['enabled'], function(data) {
-    if (data.enabled) {
+
         document.getElementsByClassName("gb-student-assignments-grid")[0].style.color = 'black';
 document.getElementById("gb-status").style.backgroundColor = 'white';
     };
-        });
-    
+
 

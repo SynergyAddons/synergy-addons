87682954637c377afe6315b2c670d2a25bfeacdf
thexpiredpear <49598383+thexpiredpear@users.noreply.github.com>
2020-02-24 22:04:06 -0800

    Update studentid.js

--- studentid.js
+++ studentid.js
@@ -1,7 +1,6 @@
-chrome.storage.local.get(['enabled'], function(data) {
-    if (data.enabled) {
+
 var studentid = document.getElementsByClassName("student-id");
 studentid[0].style.display = 'none'; //turns out apparently u need an array for class names :/ - RP
 var info = document.getElementsByClassName("info");
 info[0].style.display = 'none'; //turns out apparently u need an array for class names :/ - RP
-    };
+

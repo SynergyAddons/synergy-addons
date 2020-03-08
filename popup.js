7604f745725fb3fd78eef572f99312fdd1dd11b9
thexpiredpear <49598383+thexpiredpear@users.noreply.github.com>
2020-03-08 11:59:44 -0700

    reload page as enabled/disabled, default changed to enabled

--- popup.js
+++ popup.js
@@ -9,9 +9,10 @@ source.onclick = function(element) {
 });
     
   };
+
 var storage = chrome.storage.local;
 storage.get('enabled', function(data) {
-  if(data.enabled == 'undefined' || data.enabled == 'y') {
+  if(data.enabled == 'y'|| data.enabled == null) {
     document.getElementById('checkbox').checked = true;
   } else {
     document.getElementById('checkbox').checked = false;
@@ -23,11 +24,13 @@ window.addEventListener('change', function (){
   if(checked) {
         storage.set({'enabled':'y'},function(){
         console.log("saved");
+	chrome.tabs.reload();
       });
       
      } else {
        storage.set({'enabled':'n'},function(){
         console.log("saved");
+	chrome.tabs.reload()
       });
        
      };

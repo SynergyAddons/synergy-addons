0842e96ed70f4b226cf1e1850e5c3d9f100e8ea7
Evan Gubarev <evchik2007@gmail.com>
2020-03-10 00:10:27 -0700

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,3 +1,6 @@
+// This file is subject to the terms and conditions defined in
+// file 'LICENSE.txt', which is part of this source code package.
+// (c) 2020 Rishi P, Evan G
 var sourceclicked = document.getElementsByClassName('source');
 var source = sourceclicked[0];
 

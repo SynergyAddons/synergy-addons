d5a460d03068aa0f7cdb3eb05c72e746d8420fc9
Evan Gubarev <evchik2007@gmail.com>
2020-03-10 00:08:53 -0700

    Update assesments.js

--- assesments.js
+++ assesments.js
@@ -1,3 +1,6 @@
+// This file is subject to the terms and conditions defined in
+// file 'LICENSE.txt', which is part of this source code package.
+// (c) 2020 Rishi P, Evan G
 var storage = chrome.storage.local;
 storage.get('enabled', function(data) {
 var assesments = document.getElementsByClassName('ScheduleName');

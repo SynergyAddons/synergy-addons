9ce5d331a9a31f3ca2e9ac0d2a5ddc1183dd4f8d
Evan Gubarev <evchik2007@gmail.com>
2020-03-19 19:47:58 -0700

    Revert "added welcome page"
    This reverts commit 67b3c595542e4cccc3c31bc332944441e50bd375.

--- welcome.js
+++
@@ -1,5 +0,0 @@
-chrome.runtime.onInstalled.addListener(function (object) {
-    chrome.tabs.create({url: "http://yoursite.com/"}, function (tab) {
-        console.log("New tab launched with http://yoursite.com/");
-    });
-});

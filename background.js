4b9472193ae431869ca6a80287f5dd0198f1d26d
WickedTree <50127946+WickedTree@users.noreply.github.com>
2020-03-26 12:30:18 -0700

    Update background.js

--- background.js
+++ background.js
@@ -2,4 +2,7 @@ chrome.runtime.onInstalled.addListener(function() {
     chrome.tabs.create({'url':'welcome.html'}, function(data) {
         console.log(data);
     });
+    chrome.storage.local.set({
+        'enabled': 'y'
+    }, function() {});
 });

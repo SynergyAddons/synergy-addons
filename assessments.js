569b5975161ee8fc2902775070eac9bdea5668a5
thexpiredpear <49598383+thexpiredpear@users.noreply.github.com>
2020-03-26 18:36:34 -0700

    Update and rename assesments.js to assessments.js

--- assessments.js
+++ assessments.js
@@ -3,8 +3,8 @@
 // (c) 2020 Rishi P, Evan G
 var storage = chrome.storage.local;
 storage.get('enabled', function(data) {
-var assesments = document.getElementsByClassName('ScheduleName');
-for(i = 0; i < assesments.length; i++) {
-  assesments[i].setAttribute('style', 'color: "white"');
+var assessments = document.getElementsByClassName('ScheduleName');
+for(i = 0; i < assessments.length; i++) {
+  assessments[i].setAttribute('style', 'color: "white"');
 }
 });

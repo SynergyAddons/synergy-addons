cb0f0c8ec3ca63acac7ff2a02075d6b8ddc9b442
Evan Gubarev <evan@evangubarev.com>
2020-03-26 18:10:20 -0700

    Update a.js

--- a.js
+++ a.js
@@ -1,7 +1,7 @@
 // This file is subject to the terms and conditions defined in
 // file 'LICENSE.txt', which is part of this source code package.
 // (c) 2020 Rishi P, Evan G
-var storage = chrome.storage.loocal;
+var storage = chrome.storage.local;
 storage.get('enabled', function(data) {
 document.getElementsByClassName("gb-student-assignments-grid")[0].style.color = 'black';
 document.getElementById("gb-status").style.backgroundColor = 'white';

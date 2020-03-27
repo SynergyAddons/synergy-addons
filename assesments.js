169fdbc3ed2dab87ee156f05e86c20826d741f1c
codefactor-io <support@codefactor.io>
2020-03-27 01:29:40 +0000

    [CodeFactor] Apply fixes

--- assesments.js
+++ assesments.js
@@ -6,5 +6,5 @@ storage.get('enabled', function(data) {
 var assesments = document.getElementsByClassName('ScheduleName');
 for(i = 0; i < assesments.length; i++) {
   assements[i].setAttribute('style', 'color: "white"');
-};
+}
 });

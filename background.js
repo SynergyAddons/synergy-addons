000a2990e9650ecf7cca93d639a76a4550cfde05
Evan Gubarev <evan@evangubarev.com>
2020-03-28 16:51:31 -0700

    Update background.js

--- background.js
+++ background.js
@@ -1,3 +1,6 @@
+// This file is subject to the terms and conditions defined in
+// file 'LICENSE.txt', which is part of this source code package.
+// (c) 2020 Rishi P, Evan G, Johan N
 chrome.runtime.onInstalled.addListener(function () {
   chrome.tabs.create({ url: "welcome.html" }, function (data) {
     console.log(data)

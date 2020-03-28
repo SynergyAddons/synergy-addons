c7a1a8ff748467eb8f3cab01242b4069cfeffb00
WickedTree <50127946+WickedTree@users.noreply.github.com>
2020-03-28 00:48:04 -0700

    Fixed text not displaying

--- assessments.js
+++ assessments.js
@@ -1,10 +1,15 @@
 // This file is subject to the terms and conditions defined in
 // file 'LICENSE.txt', which is part of this source code package.
 // (c) 2020 Rishi P, Evan G
-var storage = chrome.storage.local
-storage.get("enabled", function (data) {
-  var assessments = document.getElementsByClassName("ScheduleName")
-  for (i = 0; i < assessments.length; i++) {
-    assessments[i].setAttribute("style", 'color: "white"')
-  }
-})
+
+var storage = chrome.storage.local;
+storage.get('enabled', function(data) {
+var assessments = document.getElementsByClassName('ScheduleName');
+for(i = 0; i < assessments.length; i++) {
+  assessments[i].setAttribute('style', 'color: white;');
+}
+var cssinj = document.createElement("style");
+var thecss = document.createTextNode(".text-center {color: #FFFFFF !important;}");
+cssinj.appendChild(thecss);
+document.getElementsByTagName("head")[0].appendChild(cssinj);
+});

ea4d2d49011208077a7cac69db513a4a1d0a7bed
thexpiredpear <thexpiredpear@users.noreply.github.com>
2020-03-29 17:50:27 +0000

    Prettified Code!

--- studentid.js
+++ studentid.js
@@ -1,10 +1,10 @@
 // This file is subject to the terms and conditions defined in
 // file 'LICENSE.txt', which is part of this source code package.
 // (c) 2020 Rishi P, Evan G
-var storage = chrome.storage.local;
-storage.get('enabled', function(data) {
-var studentid = document.getElementsByClassName("student-id");
-studentid[0].innerHTML = '[Student ID Hidden]';
-var info = document.getElementsByClassName("info");
-info[0].innerHTML = '[Student ID Hidden]'; 
-});
+var storage = chrome.storage.local
+storage.get("enabled", function (data) {
+  var studentid = document.getElementsByClassName("student-id")
+  studentid[0].innerHTML = "[Student ID Hidden]"
+  var info = document.getElementsByClassName("info")
+  info[0].innerHTML = "[Student ID Hidden]"
+})

db13081d8552df9daa9246b87abc8327a1dc9d7a
thexpiredpear <49598383+thexpiredpear@users.noreply.github.com>
2020-05-16 14:56:39 -0700

    Update studentid.js

--- studentid.js
+++ studentid.js
@@ -2,7 +2,7 @@
 // file 'LICENSE.txt', which is part of this source code package.
 // (c) 2020 Rishi P, Evan G, Johan N
 var storage = chrome.storage.local
-storage.get("enabled", function (data) {
+storage.get("idenabled", function (data) {
   var studentid = document.getElementsByClassName("student-id")
   studentid[0].innerHTML = "[Student ID Hidden]"
   var info = document.getElementsByClassName("info")
